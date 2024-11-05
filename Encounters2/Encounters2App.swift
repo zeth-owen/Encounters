d2fc41ee604f90e30c13a4464ad9ee09c3041298
Zeth Thomas <zeththomas89@gmail.com>
2024-11-04 22:51:09 -0800

    Initial Commit

---
+++ Encounters2/Encounters2App.swift
@@ -0,0 +1,17 @@
+//
+//  Encounters2App.swift
+//  Encounters2
+//
+//  Created by Zeth Thomas on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Encounters2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Encounters2Tests/Encounters2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Encounters2Tests.swift
+//  Encounters2Tests
+//
+//  Created by Zeth Thomas on 11/4/24.
+//
+
+import Testing
+@testable import Encounters2
+
+struct Encounters2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
